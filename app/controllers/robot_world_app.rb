9e25535b1e8039f859128aac6e9aa2d2e1b6473e
mimilettd <mimilettd@gmail.com>
2017-08-15 23:46:35 -0600

    Initial commit

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,7 +4,26 @@ class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params)
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  not_found do
+    erb :error
+  end
+end
